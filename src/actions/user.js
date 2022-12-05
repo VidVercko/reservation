00111b40e8f053b9cf1416de22a2701b4c43f76d
MihaKr <75996374+MihaKr@users.noreply.github.com>
2022-12-05 17:34:06 +0100

    IOS
    Vse kar je treba, da se na ios pržge

--- src/actions/user.js
+++ src/actions/user.js
@@ -3,6 +3,7 @@ import { USER_LOGIN_SUCCESS, USER_LOGIN_START, USER_REGISTER_START, USER_REGISTE
 export const userLogin = ({ }) => {
     return (dispatch) => {
         dispatch({ type: USER_LOGIN_START });
+        
         dispatch({ type: USER_LOGIN_SUCCESS, payload: { jwt: 'fake-jwt', user: { name: 'lorem ipsum' } } });
     }
 }
